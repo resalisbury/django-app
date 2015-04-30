ff11e1b01371d07d469db8cd61211bba2438153e
Rex Salisbury <rexsalisbury@Rex-Ss-MacBook-Pro.local>
2015-04-30 12:19:48 -0700

    add export to csv button to polls/index.html

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,6 +11,7 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
+		now = timezone.now()
 		return self.pub_date >= timezone.now()
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
