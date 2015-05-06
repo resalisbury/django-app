a33f893a6dd41353a0cab12aee5398ea3dc866b5
resalisbury <rex.salisbury@gmail.com>
2015-05-06 09:01:39 -0700

    being implementation of slug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,9 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-	question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	slug = modesl.SlugField(unique=True)
 
 	def __str__(self):
 		return self.question_text
@@ -16,6 +17,9 @@ class Question(models.Model):
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
+
+	def get_absolute_url(self):
+		return reverse('detail', kwargs={'slug': self.slug})
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, url
 
 from polls import views
 
-urlpatterns = patterns('',
-	url(r'^export$', views.export, name='index'),
-	url(r'^$', views.IndexView.as_view(), name='export'),
-	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
-	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+urlpatterns = patterns(
+    '',
+    url(r'^export$', views.export, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='export'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
-
