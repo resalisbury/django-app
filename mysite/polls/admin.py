e10037d668a16c52d19f7e4a1cec2f0af8858e25
resalisbury <rex.salisbury@gmail.com>
2015-05-08 16:02:25 -0700

    pep8

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,15 +5,16 @@ from django.http import HttpResponse
 
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.TabularInline):
-	model = Choice
-	extra = 3
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 
-        'classes': ['collapse']}),
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
@@ -29,20 +30,17 @@ class QuestionAdmin(admin.ModelAdmin):
         f = open('questions.csv', 'wb')
         writer = csv.writer(f)
         writer.writerow(self.list_display)
-     
+
         for q in queryset:
-            print q.was_published_recently == True
             writer.writerow([q.id, q.question_text, q.was_published_recently()])
-     
+
         f.close()
-     
+
         f = open('questions.csv', 'r')
         response = HttpResponse(f, content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=stat-info.csv'
         return response
 
     download_csv.short_description = "Download CSV file for selected stats."
-
-
 
 admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,30 +3,32 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
-	slug = modesl.SlugField(unique=True)
+    pub_date = models.DateTimeField('date published')
+    slug = modesl.SlugField(unique=True)
 
-	def __str__(self):
-		return self.question_text
+    def __str__(self):
+        return self.question_text
 
-	def was_published_recently(self):
-		now = timezone.now()
-		return self.pub_date >= timezone.now()
-	was_published_recently.admin_order_field = 'pub_date'
-	was_published_recently.boolean = True
-	was_published_recently.short_description = 'Published recently?'
+    def was_published_recently(self):
+        now = timezone.now()
+        return self.pub_date >= timezone.now()
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
-	def get_absolute_url(self):
-		return reverse('detail', kwargs={'slug': self.slug})
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'slug': self.slug})
+
 
 class Choice(models.Model):
-	question = models.ForeignKey(Question)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.choice_text
+    def __str__(self):
+        return self.choice_text
 
 datetime.timedelta(days=1)
