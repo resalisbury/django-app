48c04245889a7fffb8fb4aac459904cdecd784e8
resalisbury <rex.salisbury@gmail.com>
2015-05-14 12:08:32 -0700

    rename function to rename upload file

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-def new_file_name(instance, filename):
+def get_new_file_name(instance, filename):
     return '/'.join(['chatJSON', 'chat-v' + str(instance.version) + '.json'])
 
 
 class ChatJSON(models.Model):
-    script = models.FileField(upload_to=new_file_name)
+    script = models.FileField(upload_to=get_new_file_name)
     version = models.DecimalField(max_digits=6, decimal_places=2, unique=True)
 
     def __str__(self):
