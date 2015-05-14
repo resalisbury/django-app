43a6946ac930c0a6d55e684bd599ea1175bf349a
resalisbury <rex.salisbury@gmail.com>
2015-05-14 10:07:46 -0700

    add in admin for ChatJSON; create models for ChatJSON

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class ChatJSON(models.Model):
-    script = models.FileField 
+    script = models.FileField(upload_to='chatJSON')
     version = models.DecimalField(max_digits=6, decimal_places=2)
