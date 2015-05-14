1a688faf78434b8ebb8af6fb702e98e39009a56d
resalisbury <rex.salisbury@gmail.com>
2015-05-14 14:34:06 -0700

    ChatDetail returns correct version

--- mysite/chat/api_urls.py
+++ mysite/chat/api_urls.py
@@ -5,5 +5,5 @@ from chat import api_views
 urlpatterns = patterns(
     '',
     url(r'^$', api_views.ChatList.as_view(), name="chat_rest_api"),
-    url(r'^v(?P<version>\d+\.?\d{1,2})$', api_views.ChatList.as_view(), name="chat_rest_api"),
+    url(r'^v(?P<version>\d+\.?\d{1,2})$', api_views.ChatDetail.as_view(), name="chat_rest_api"),
 )
--- mysite/chat/api_views.py
+++ mysite/chat/api_views.py
@@ -6,3 +6,9 @@ from chat.models import ChatJSON
 class ChatList(generics.ListCreateAPIView):
     queryset = ChatJSON.objects.all()
     serializer_class = ChatSerializer
+
+
+class ChatDetail(generics.RetrieveAPIView):
+    queryset = ChatJSON.objects.all()
+    serializer_class = ChatSerializer
+    lookup_field = 'version'
