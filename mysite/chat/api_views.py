7f714851e2d5fbe15fcf23db20143fd8f6f490d9
resalisbury <rex.salisbury@gmail.com>
2015-05-14 16:09:33 -0700

    api returns json for guids

--- mysite/chat/api_views.py
+++ mysite/chat/api_views.py
@@ -1,4 +1,11 @@
+import json
+
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.core.files import File
+
 from chat.serializers import ChatSerializer
 from chat.models import ChatJSON
 
@@ -12,3 +19,29 @@ class ChatDetail(generics.RetrieveAPIView):
     queryset = ChatJSON.objects.all()
     serializer_class = ChatSerializer
     lookup_field = 'version'
+
+
+class ChatModule(APIView):
+
+    def get(self, request, guid, version, format=None):
+        chat = ChatJSON.objects.get(version=float(version))
+        script = chat.script
+
+        with open(script.path, 'r') as data_file:
+            data = json.load(data_file)
+
+        response = self.get_guid(guid, data)
+
+        return Response(response)
+
+    def get_guid(self, guid, data):
+        steps = self.get_steps(data['modules'])
+        for step in steps:
+            if step['guid'] == guid:
+                return step
+
+    def get_steps(self, modules):
+        steps = []
+        for module in modules:
+            steps += module['steps']
+        return steps
