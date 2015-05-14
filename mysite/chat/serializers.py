751d00a30bd65b375c8e3728c98333e3bed2a2bc
resalisbury <rex.salisbury@gmail.com>
2015-05-14 13:52:59 -0700

    represent version as number

--- mysite/chat/serializers.py
+++ mysite/chat/serializers.py
@@ -3,6 +3,7 @@ from chat.models import ChatJSON
 
 
 class ChatSerializer(serializers.ModelSerializer):
+    version = serializers.DecimalField(max_digits=6, decimal_places=2, coerce_to_string=False)
 
     class Meta:
         model = ChatJSON
