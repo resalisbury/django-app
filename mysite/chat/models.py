e1e9dfab7edcbd8d4c09492f159033d442d00c28
resalisbury <rex.salisbury@gmail.com>
2015-05-14 11:24:04 -0700

    add str method for ChatJSON model

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -8,3 +8,6 @@ def new_file_name(instance, filename):
 class ChatJSON(models.Model):
     script = models.FileField(upload_to=new_file_name)
     version = models.DecimalField(max_digits=6, decimal_places=2, unique=True)
+
+    def __str__(self):
+        return 'Chat v-%s' % str(self.version)
