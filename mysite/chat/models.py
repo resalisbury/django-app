7bf6d05a19bd7cfa53751e2b20b06fd75e04ca2c
resalisbury <rex.salisbury@gmail.com>
2015-05-14 11:05:43 -0700

    change filename on upload

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 
+def new_file_name(instance, filename):
+    return '/'.join(['chatJSON', 'chat-v' + str(instance.version) + '.json'])
+
+
 class ChatJSON(models.Model):
-    script = models.FileField(upload_to='chatJSON')
-    version = models.DecimalField(max_digits=6, decimal_places=2)
+    script = models.FileField(upload_to=new_file_name)
+    version = models.DecimalField(max_digits=6, decimal_places=2, unique=True)
